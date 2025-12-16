9ef2014b1e53b1e9f10f57810e470b5d8fb22a7a
Saidu M Koroma <saidumkoroma99@gmail.com>
2025-12-16 07:25:54 -0800

    First Update

--- script.js
+++ script.js
@@ -9,6 +9,11 @@ burger.onclick =()=>{
 
 
 
+
+ 
+
+
+
 function like(){
     let likeButton1 = document.querySelector("#heart1");
     let likeButton2 = document.querySelector("#heart2");
@@ -16,6 +21,12 @@ function like(){
     let likeButton4 = document.querySelector("#heart4");
     let likeButton5 = document.querySelector("#heart5");
     let likeButton6 = document.querySelector("#heart6");
+    let likeButton7 = document.querySelector("#heart8");
+    let likeButton8 = document.querySelector("#heart9");
+    let likeButton9 = document.querySelector("#heart10");
+    let likeButton10 = document.querySelector("#heart11");
+    let likeButton11 = document.querySelector("#heart12");
+    let likeButton12 = document.querySelector("#heart7");
 
     likeButton1.onclick =()=>{
         likeButton1.classList.toggle("like");
@@ -36,5 +47,27 @@ function like(){
     likeButton6.onclick =()=>{
         likeButton6.classList.toggle("like");
     }
+
+
+     likeButton7.onclick =()=>{
+        likeButton7.classList.toggle("like");
+    }
+
+    likeButton8.onclick =()=>{
+        likeButton8.classList.toggle("like");
+    }
+    likeButton9.onclick =()=>{
+        likeButton9.classList.toggle("like");
+    }
+    likeButton10.onclick =()=>{
+        likeButton10.classList.toggle("like");
+    }
+    likeButton11.onclick =()=>{
+        likeButton11.classList.toggle("like");
+    }
+    likeButton12.onclick =()=>{
+        likeButton12.classList.toggle("like");
+    }
 }
+
 like();
